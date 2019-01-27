dd6760f0ae05f0da9486fbf5cfb10cd0d35fad9c
GUY Jérôme <scoonydeus@gmail.com>
2019-01-28 00:02:14 +0100

    Scoony

--- ifttt-control.sh
+++ ifttt-control.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+## Config
+sync_folder=""
+
+## Reboot
+if [[ -f "$sync_folder/reboot.jpg" ]]; then
+  rm "$sync_folder/reboot.jpg"
+  reboot
+fi
+
+## Update
+if [[ -f "$sync_folder/update.jpg" ]]; then
+  rm "$sync_folder/update.jpg"
+  apt update
+  apt upgrade
+  apt install -f
+  apt autoremove
+fi
